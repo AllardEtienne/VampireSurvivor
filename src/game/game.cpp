25a849a16d07da3a91ef32a4467b5003856b5dd2
AllardEtienne <etienne.allard@ynov.com>
2025-12-02 16:56:54 +0100

    annimation
    annimation good

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,6 +1,15 @@
 #include "game/game.h"
 #include "common/common.h"
 
+enum class direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Idle
+};
+
 
 namespace game::ui
 {
@@ -9,32 +18,30 @@ int widthWindow = 800, heightWindow = 600;
 void moveUp(sf::Sprite& sprite)
 {
     sprite.move(0.f, -5.f);
-    sprite.setTextureRect(sf::IntRect(0, 352, 32, 32));
 }
 void moveDown(sf::Sprite& sprite)
 {
     sprite.move(0.f, 5.f);
-    sprite.setTextureRect(sf::IntRect(0, 160, 32, 32));
 }
 void moveLeft(sf::Sprite& sprite)
 {
     sprite.move(-5.f, 0.f);
-    sprite.setTextureRect(sf::IntRect(0, 224, 32, 32));
 }
 void moveRight(sf::Sprite& sprite)
 {
     sprite.move(5.f, 0.f);
-    sprite.setTextureRect(sf::IntRect(0, 288, 32, 32));
-}
-void idle(sf::Sprite& sprite)
-{
-    sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
 }
 
 
 
  std::expected<int, std::string> runGame()
 {
+     direction lastDirection = direction::Idle;
+     direction currentDirection = direction::Idle;
+     direction lookingDirection = direction::Down;
+     int frameCounter = 0;
+     
+
     sf::Texture playerTexture;
     if (!playerTexture.loadFromFile("assets/player.png"))
     {
@@ -44,18 +51,13 @@ void idle(sf::Sprite& sprite)
     playerTexture.setSmooth(true);
     sf::Sprite playerSprite;
     playerSprite.setTexture(playerTexture);
-    playerSprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
-    playerSprite.setScale(2.f, 2.f);
     playerSprite.setPosition(widthWindow / 2.f, heightWindow / 2.f);
-    
 
-
-
-    // Création d'une fenêtre "render"
+    // Création d'une fenêtre 
     sf::RenderWindow window(sf::VideoMode(widthWindow, heightWindow), "VampireSurvivor",
                             sf::Style::Default); // Pour un style personnalisé
     
-    window.setFramerateLimit(60);
+    window.setFramerateLimit(30);
 
 
     // Boucle principale avec gestion frame par frame
@@ -81,25 +83,34 @@ void idle(sf::Sprite& sprite)
                 }
                 else if (event.key.code == sf::Keyboard::D or event.key.code == sf::Keyboard::Right)
                 {
+                    currentDirection = direction::Right;
+                    lookingDirection = direction::Right;
                     moveRight(playerSprite);
                 }
                 else if (event.key.code == sf::Keyboard::Q or event.key.code == sf::Keyboard::Left)
                 {
+                    currentDirection = direction::Left;
+                    lookingDirection = direction::Left;
                     moveLeft(playerSprite);
                 }
                 else if (event.key.code == sf::Keyboard::Z or event.key.code == sf::Keyboard::Up)
                 {
+                    currentDirection = direction::Up;
+                    lookingDirection = direction::Up;
                     moveUp(playerSprite);
                 }
                 else if (event.key.code == sf::Keyboard::S or event.key.code == sf::Keyboard::Down)
                 {
+                    currentDirection = direction::Down;
+                    lookingDirection = direction::Down;
                     moveDown(playerSprite);
                 }
                 else
                 {
-                    idle(playerSprite);
+                    currentDirection = direction::Idle;
                 }
             }
+
 
             // D'autres évènements peuvent être gérés ici
         }
@@ -110,10 +121,62 @@ void idle(sf::Sprite& sprite)
         window.clear(sf::Color::Green);
 
         // Dessiner les éléments du menu
+        if (currentDirection == lastDirection)
+        {
+            frameCounter++;
+        }
+        else
+        {
+            frameCounter = 0;
+        }
+
+        if (currentDirection == direction::Right)
+        {
+            playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 9, 32, 32));
+        }
+        else if (currentDirection == direction::Left)
+        {
+            playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 7, 32, 32));
+        }
+        else if (currentDirection == direction::Up)
+        {
+            playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 11, 32, 32));
+        }
+        else if (currentDirection == direction::Down)
+        {
+            playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 5, 32, 32));
+        }
+        else if (currentDirection == direction::Idle)
+        {
+            if (lookingDirection == direction::Right)
+            {
+                playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 1, 32, 32));
+            }
+            else if (lookingDirection == direction::Left)
+            {
+                playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 2, 32, 32));
+            }
+            else if (lookingDirection == direction::Up)
+            {
+                playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 3, 32, 32));
+            }
+            else if (lookingDirection == direction::Down)
+            {
+                playerSprite.setTextureRect(sf::IntRect((frameCounter / 10) % 4 * 32, 32 * 0, 32, 32));
+            }
+        }
+        else
+       
+        playerSprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+        playerSprite.setScale(2.f, 2.f);
+
+
         window.draw(playerSprite);
 
         // Affiche la nouvelle frame à l'écran
         window.display();
+        lastDirection = currentDirection;
+        
     }
     return 0;
 }
--- src/game/game.h
+++ src/game/game.h
@@ -12,7 +12,6 @@ GAME_EXPORT void moveUp(sf::Sprite& sprite);
 GAME_EXPORT void moveDown(sf::Sprite& sprite);
 GAME_EXPORT void moveLeft(sf::Sprite& sprite);
 GAME_EXPORT void moveRight(sf::Sprite& sprite);
-GAME_EXPORT void idle(sf::Sprite& sprite);
 
 
 }; 
